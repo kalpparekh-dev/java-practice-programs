c50d90ae53265251df4a7a5b8b10ab0131b6ddeb
Parekh Kalp <kalpparekh22@gmail.com>
2026-03-13 18:22:05 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Kalp Parekh.");
+        System.out.println("I am learning Java and building projects.");
+    }
+}
